09f4a08dfec481d6097350e1c8c5945eb0389eac
unknown <su-masuda@usengroup.com>
2020-09-21 16:56:52 +0900

    PickupList作成

--- src/Recochoku/index.jsx
+++ src/Recochoku/index.jsx
@@ -1,11 +1,14 @@
 // libs
 import React from "react";
+// components
+import PickupList from "./mains/PickupList";
 // others
 import "./style.scss";
 
 const Recochoku = () => (
   <div className="recochoku-wrapper">
     <p>Reactの勉強</p>
+    <PickupList />
   </div>
 );
 
---
+++ src/Recochoku/mains/PickupList/index.jsx
@@ -0,0 +1,8 @@
+// libs
+import React from "react";
+// others
+import "./style.scss";
+
+const PickupList = () => <div>PickupList</div>;
+
+export default PickupList;
