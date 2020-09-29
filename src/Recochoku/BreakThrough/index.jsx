e792fe667cf3d971f75fc4960c152cf87540ec45
Su Masuda <su-masuda@usen-next.jp>
2020-09-29 17:22:12 +0900

    RecommendSongContainerフォルダ作成

---
+++ src/Recochoku/BreakThrough/index.jsx
@@ -0,0 +1,20 @@
+// // libs
+// import React from "react";
+// // components
+// import RecommendSongContainer from "../../commons/RecommendSongContainer";
+// // others
+// import "./style.scss";
+
+// const recommendSongList = [
+//     {
+//         recommendSongTitle="ACOMPLETE~ALL SINGLES~",
+//         recommendSongSinger="浜崎あゆみ",
+//         recommendSongPrice="916"
+//     }
+// ]
+
+// const BreakThough = () => (
+//     <RecommendSongContainer recommendSongList={recommendSongList} />
+// );
+
+// export default BreakThrough;
